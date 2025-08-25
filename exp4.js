5b09df53dc6755208f66a3666b1f3a877c47d54c
chand-hussain <u23cse1051@cit.ac.in>
2025-08-25 11:36:57 +0530

    Add files via upload

---
+++ exp4.js
@@ -0,0 +1,62 @@
+const newbtn1 = document.querySelector("#btn1");
+newbtn1.addEventListener("click",()=>
+    {
+document.querySelector(".Item1").style.backgroundImage = "url('Fruit1.jpg')";
+document.querySelector(".Item2").style.backgroundImage = "url('Fruit2.jpg')";
+document.querySelector(".Item3").style.backgroundImage = "url('f3.jpg')";
+document.querySelector(".Item4").style.backgroundImage = "url('f4.jpg')";
+document.querySelector(".Item5").style.backgroundImage = "url('f5.jpg')";
+
+    });
+
+// For div2(Food)//
+
+const newbtn2 = document.querySelector("#btn2");
+newbtn2.addEventListener("click",()=>
+    {
+document.querySelector(".f1").style.backgroundImage = "url('food1.jpg')";
+document.querySelector(".f2").style.backgroundImage = "url('food2.jpg')";
+document.querySelector(".f3").style.backgroundImage = "url('food3.jpg')";
+document.querySelector(".f4").style.backgroundImage = "url('food4.jpg')";
+document.querySelector(".f5").style.backgroundImage = "url('food5.jpg')";
+
+    });
+
+
+//for div3 //
+
+const newbtn3 = document.querySelector("#btn3");
+newbtn3.addEventListener("click",()=>
+    {
+document.querySelector(".b1").style.backgroundImage = "url('b1.jpg')";
+document.querySelector(".b2").style.backgroundImage = "url('b2.jpg')";
+document.querySelector(".b3").style.backgroundImage = "url('b3.jpg')";
+document.querySelector(".b4").style.backgroundImage = "url('b4.jpg')";
+document.querySelector(".b5").style.backgroundImage = "url('b5.jpg')";
+
+    });
+
+// for div4//
+const newbtn4 = document.querySelector("#btn4");
+newbtn4.addEventListener("click",()=>
+    {
+document.querySelector(".a1").style.backgroundImage = "url('a1.jpg')";
+document.querySelector(".a2").style.backgroundImage = "url('a2.jpg')";
+document.querySelector(".a3").style.backgroundImage = "url('a3.jpg')";
+document.querySelector(".a4").style.backgroundImage = "url('a4.jpg')";
+document.querySelector(".a5").style.backgroundImage = "url('a5.jpg')";
+
+    });
+
+//for  div5//
+const newbtn5 = document.querySelector("#btn5");
+newbtn5.addEventListener("click",()=>
+    {
+document.querySelector(".s1").style.backgroundImage = "url('s1.jpg')";
+document.querySelector(".s2").style.backgroundImage = "url('s2.jpg')";
+document.querySelector(".s3").style.backgroundImage = "url('s3.jpg')";
+document.querySelector(".s4").style.backgroundImage = "url('s4.jpg')";
+document.querySelector(".s5").style.backgroundImage = "url('s5.jpg')";
+
+    });
+
